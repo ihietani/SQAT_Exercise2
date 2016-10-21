5a39583b1a1513bf253f1a959733503eafde7999
somename <someemail>
2016-10-21 15:45:28 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -22,5 +22,10 @@ public class TestPlanetExplorer {
 		PlanetExplorer pe = new PlanetExplorer(100,100,"(5,5)(7,8)");
 		assertEquals("The position is incorrect", "(0,2,N)", pe.executeCommand("ff"));
 	}
+	@Test
+	public void test_moveEast(){
+		PlanetExplorer pe = new PlanetExplorer(100,100,"(5,5)(7,8)");
+		assertEquals("The position is incorrect", "(0,2,N)", pe.executeCommand("ffe"));
+	}
 	
 }
