645cc892310654b366c3ab87eaf5bc6542c8cc31
somename <someemail>
2016-10-21 15:04:18 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -14,6 +14,6 @@ public class TestPlanetExplorer {
 	public void test_executeCommandEast() {
 		//
 		PlanetExplorer pe = new PlanetExplorer(100,100,"(5,5)(7,8)");
-		assertEquals("Explorer did not move", "(0,1,N)", pe.executeCommand("r"));
+		assertEquals("Explorer did not move", "(0,0,E)", pe.executeCommand("r"));
 	}
 }
